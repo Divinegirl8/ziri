818ccc18b5714280aafb2fa9276a15c1d56649ad
Divinegirl8 <iamoluchimercy@gmail.com>
2025-07-19 18:35:12 +0100

    new commit

--- src/pages/FinanceHomePage.tsx
+++ src/pages/FinanceHomePage.tsx
@@ -1,22 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import {Sidebar} from "../component/sidebar/Sidebar.tsx";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import { Sidebar } from "../component/sidebar/Sidebar.tsx";
+import axios, { AxiosError } from "axios";
+import type { UserData } from '../interface.ts';
 
 
-export default function FinanceHomePage() {
+
+const  FinanceHomePage:React.FC = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [userData, setUserData] = useState(null);
+    const [userData, setUserData] = useState<UserData | null>(null);
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const { data } = await axios.get('https://gooziri.onrender.com/users/me');
+                const { data } = await axios.get<UserData>('https://gooziri.onrender.com/users/me');
                 setUserData(data);
             } catch (error) {
                 console.error('Error fetching user data:', error);
-                if (error.response && error.response.status === 401) {
-                    // window.location.href = '/signIn';
-                    console.error('Error fetching user data:', error);
+                
+            
+                if (error instanceof AxiosError && error.response?.status === 401) {
+                    console.error('Unauthorized access:', error);
                 } else {
                     alert('Failed to fetch user data. Please try again later.');
                 }
@@ -30,12 +33,14 @@ export default function FinanceHomePage() {
             <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
             <main className="flex-1 p-8 overflow-y-auto">
                 <h1 className="text-4xl font-bold text-gray-800">Gooziri finance Dashboard</h1>
-                {  userData && (
+                {userData && (
                     <p>
-                        {userData.fullName}
+                        Welcome, {userData.fullName}
                     </p>
                 )}
             </main>
         </div>
     );
 }
+
+export default FinanceHomePage
